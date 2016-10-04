f0ff0a0200bdb08ccaff90ff318efc2c01cdfc4a
jesse soldat <jsoldat@hotmail.com>
2016-10-04 11:05:16 +0700

    bind

--- main.js
+++ main.js
@@ -5,4 +5,51 @@ let dog = {
 	}
 }
 
-dog.talk();
+// dog.talk();
+
+let talkFunction = dog.talk;
+let boundFunction = talkFunction.bind(dog);
+
+
+// talkFunction()
+// boundFunction()
+
+let button = document.getElementById('clickMe');
+
+button.addEventListener('click', dog.talk.bind(dog))
+
+let pig = function() {
+	console.log(this.sound);
+}
+let oinky = {
+	sound: 'oink'
+}
+let pigBound = pig.bind(oinky);
+// pig()
+// pigBound()
+
+
+let sheep = function() {
+	console.log(this.sound);
+}
+let sheepy = {
+	speak: sheep,
+	sound: 'baa'
+}
+// sheepy.speak();
+
+let cow = function(){
+	console.log(this.sound);
+}
+
+let mooy = {
+	sound: 'moo'
+}
+
+mooy.speak = cow.bind(mooy)
+
+mooy.speak()
+
+
+
+
